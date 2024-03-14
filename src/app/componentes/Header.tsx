77948018c812a71abaee0c6b5ff6e6b1cf3b7ce9
Victor Hugo <victorzarbesu@gmail.com>
2024-03-14 08:25:23 -0300

    Componente Header e Footer

---
+++ src/app/componentes/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () =>{
+    return(
+        <>
+            <header>
+                <p>Aqui vai o Header</p>
+            </header>
+        </>
+    )
+};
+
+export default Header;
